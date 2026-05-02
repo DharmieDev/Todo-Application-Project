8cb2f3183e2c72afd1412f2f2d34ad2e17994fe0
Dharmie Folorunso <Dharmelorlahfolorunso@gmail.com>
2026-05-02 14:33:28 +0100

    feat: updated App.tsx file to typescript

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { lazy, Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+import { ErrorFallback } from './components/ErrorFallback'
+import { Route, Routes } from 'react-router'
+import NavBar from './components/NavBar'
+import { useState } from 'react'
+import AddTodo from './pages/AddTodo'
+import ProtectedRoute from './components/ProtectedRoute'
+
+const TodoPage = lazy(() => import("./pages/TodoPage"))
+const TodoDetails = lazy(() => import("./pages/TodoDetails"))
+const Login = lazy(() => import("./pages/Login"))
+const Register = lazy(() => import("./pages/Register"))
+const NotFound = lazy(() => import("./pages/NotFound"))
+
+
+function App() {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+
+  return (
+    <div>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+          <div>
+            <NavBar
+              page={page} setPage={setPage}
+              search={search} setSearch={setSearch}
+              searchInput={searchInput} setSearchInput={setSearchInput}
+            />
+            <Routes>
+              <Route path="/" element={
+                <ProtectedRoute>
+                  <TodoPage
+                    page={page} setPage={setPage}
+                    search={search} setSearch={setSearch}
+                    searchInput={searchInput} setSearchInput={setSearchInput}
+                  />
+                </ProtectedRoute>
+              }>
+                <Route path="/task/:id" element={<TodoDetails />} />
+                <Route path='/add' element={<AddTodo />} />
+              </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  )
+}
+
+export default App
